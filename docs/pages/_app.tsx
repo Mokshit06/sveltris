1f7d9c002e9cc175ac00bd446e7662a5c58b41d0
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2023-04-13 23:46:53 +0530

    setup @vercel/analyics

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,12 @@
 import type { AppProps } from 'next/app';
 import '../styles/index.css';
+import { Analytics } from '@vercel/analytics/react';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
